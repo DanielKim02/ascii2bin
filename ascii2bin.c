5cb4c53a1b061e946ec420f6af456be0652f3b43
Daniel Kim <daniel.kim.313@my.csun.edu>
2021-03-10 20:45:02 -0800

    help

--- ascii2bin.c
+++ ascii2bin.c
@@ -7,10 +7,10 @@
 #include <unistd.h>
 
 int main () {
-	offset = 2;
-    number = 0;
-    
-    retval = read(0, &ascii_value, 1);
+	int offset = 2;
+    int number = 0;
+    int digit = 0;
+    int retval = read(0, &ascii_value, 1);
     while (retval == 1)
         digit = ascii_value - offset;
         number = (number << 1) + digit;  
