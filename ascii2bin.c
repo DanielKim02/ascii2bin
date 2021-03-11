643580c850062bd3a5973e0a3a99088c47ec60de
Daniel Kim <daniel.kim.313@my.csun.edu>
2021-03-10 22:37:11 -0800

    almost?

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,4 +1,4 @@
-/*
+
 #include "stdio.h"
 #include "stdlib.h"
 #include <unistd.h>
@@ -11,16 +11,14 @@ int main () {
     int number = 0;
     int digit;
     char ascii_value;
-    //////////
-    scanf("%c",&ascii_value);
-    //////////
+    
     int retval = read(0, &ascii_value, 1);
-    while (((retval == 1) ||(retval == 0)) && (ascii_value != '\n')){
+    while ((retval == 1) && (ascii_value != '\n')){
         digit = ascii_value - offset;
         if((digit ==1) || (digit == 0)){
             number = (number << 1) + digit;
         }
-        else{
+        else if(digit != 1 && digit !=0){
             return 1;
         }  
         retval = read(0, &ascii_value, 1);
@@ -32,33 +30,3 @@ int main () {
     printf("%u\n", number);
     return 0;
 }
-*/
-
-
-
-//THIS IS NOT MY WORK, THIS IS A PEERS WORK, WHO I AM NOT TAKING ANY CREDIT FOR AND
-// I AM  TEMPORARILY PUSHING IT TO THE SANDBOX SERVER TO COMPARE OUR TWO PROGRAMS
-//
-#include "stdio.h"
-#include "stdlib.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-int main()
-{
-    int offset = 48;  
-    int number = 0;
-    char ascii_value;
-    int retval = read(0, &ascii_value, 1);
-    while ((retval == 1) && (ascii_value != '\n')) {
-        int digit = ascii_value - offset;
-        if ((digit == 1) || (digit == 0))
-            number = (number << 1) + digit;
-        retval = read(0, &ascii_value, 1);
-    }
-    printf("%u\n", number);
-    return 0;
-}
-
-
