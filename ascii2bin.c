16f0dd9aaaf151aee282eb362548dc82b99eabc0
Daniel Kim <daniel.kim.313@my.csun.edu>
2021-03-10 22:44:09 -0800

    Finished

--- ascii2bin.c
+++ ascii2bin.c
@@ -13,7 +13,7 @@ int main () {
     char ascii_value;
     
     int retval = read(0, &ascii_value, 1);
-    while ((retval == 1) && (ascii_value != '\n')){
+    while (retval == 1 && ascii_value != '\n'){
         digit = ascii_value - offset;
         if((digit ==1) || (digit == 0)){
             number = (number << 1) + digit;
