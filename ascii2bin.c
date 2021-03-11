127026d712fbd49dde67b3d3c8ea215efb955dbe
Daniel Kim <daniel.kim.313@my.csun.edu>
2021-03-10 20:35:24 -0800

    I dont get it yet

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,4 +1,20 @@
-//test
-void main(){
-	printf("Hello World");
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+int main () {
+	offset = 2;
+    number = 0;
+    
+    retval = read(0, &ascii_value, 1);
+    while (retval == 1)
+        digit = ascii_value - offset;
+        number = (number << 1) + digit;  
+        retval = read(0, &ascii_value, 1);
+        
+    printf("%u\n", number);
+    return 0;
 }
